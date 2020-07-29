97aa62585a8cab376f68a810b5b35484b0e98fd8
tobymould <toby.mould@icloud.com>
2020-07-29 11:07:49 +0100

    API call and save to State based on user input search term set up operational

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,34 @@ import styles from './App.scss';
 
 import Card from './Components/Card';
 
-//  c9481b
 class App extends Component {
   state = {
     searchTerm: null,
-    searchResult: {}
+    searchTermApiResponse: {}
   };
 
   setSearchTerm = event => {
+    console.log(event.target.value);
     this.setState({
       searchTerm: event.target.value
     });
   };
 
-  getAPIDataBySearchTerm = event => {
-    // event.preventDefault;
+  getApiDataBySearchTerm = async event => {
+    event.preventDefault();
+    const { searchTerm } = this.state;
+
+    const omdbApiResponse = await fetch(`http://www.omdbapi.com/?s=${searchTerm}&apikey=c9481b`);
+    const omdbApiData = await omdbApiResponse.json();
+    console.log(omdbApiData);
+    await this.setState({ searchTermApiResponse: omdbApiData });
     // return null;
   };
 
   render() {
     return (
       <div className={styles.appWrapper}>
-        <form onSubmit={this.getAPIDataBySearchTerm}>
+        <form onSubmit={this.getApiDataBySearchTerm}>
           <label htmlFor="">
             Please input your search term
             <input type="text" name="search" onChange={this.setSearchTerm} />
