c164b5715bba9059688d506d4b5fbe78fc64f42e
tobymould <toby.mould@icloud.com>
2020-07-29 10:49:16 +0100

    setup the form, the onchange setState to save the search term and skeleton of the onSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,26 @@ class App extends Component {
     searchResult: {}
   };
 
-  searchData = () => {};
+  setSearchTerm = event => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  };
+
+  getAPIDataBySearchTerm = event => {
+    // event.preventDefault;
+    // return null;
+  };
 
   render() {
     return (
       <div className={styles.appWrapper}>
+        <form onSubmit={this.getAPIDataBySearchTerm}>
+          <label htmlFor="">
+            Please input your search term
+            <input type="text" name="search" onChange={this.setSearchTerm} />
+          </label>
+        </form>
         <Card />
       </div>
     );
