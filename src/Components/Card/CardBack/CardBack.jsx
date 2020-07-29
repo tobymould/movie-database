5840f3b04ba1b0f94515814b6ea5ecbaf366480f
tobymould <toby.mould@icloud.com>
2020-07-29 17:00:21 +0100

    included all the additional information relevent to a user on the back of each card, with some stylistic improvements

--- src/Components/Card/CardBack/CardBack.jsx
+++ src/Components/Card/CardBack/CardBack.jsx
@@ -7,7 +7,36 @@ export default class CardBack extends Component {
     console.log(specificFilmDetails);
     return (
       <div className={styles.cardBackWrapper}>
-        <p>{specificFilmDetails.Plot}</p>
+        <p>
+          <span>Plot Line: </span>
+          {specificFilmDetails.Plot}
+        </p>
+
+        <div className={styles.badges}>
+          <div>{specificFilmDetails.Rated}</div>
+          <div>{specificFilmDetails.imdbRating}</div>
+          <div>{specificFilmDetails.Runtime}</div>
+        </div>
+        <p>
+          <span>AWARDS: </span>
+          {specificFilmDetails.Awards}
+        </p>
+        <p>
+          <span>Genres: </span>
+          {specificFilmDetails.Genre}
+        </p>
+        <p>
+          <span>Director: </span>
+          {specificFilmDetails.Director}
+        </p>
+        <p>
+          <span>Actors: </span>
+          {specificFilmDetails.Actors}
+        </p>
+        <p>
+          <span>Language: </span>
+          {specificFilmDetails.Language}
+        </p>
       </div>
     );
   }
